8b6feb92f59210870313c115664684b4427f15f8
Michael J. Schiumo <mschiumo18@gmail.com>
2019-02-05 16:39:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
           links.each do |link|
             if link.include?("linkedin")
               student[:linkedin] = link 
-            if link.include?("github")
+            elsif link.include?("github")
               student[:github] = link 
             if link.include?("twitter")
               student[:twitter] = link 
