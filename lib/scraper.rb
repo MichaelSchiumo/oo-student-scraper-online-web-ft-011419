e74000409f548dbad43555287906cfdc98c84e97
Michael J. Schiumo <mschiumo18@gmail.com>
2019-02-05 16:36:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
-      profile_page.css(".social-icon-container").children.css("a").map {|el| el 
+      profile_page.css(".social-icon-container").children.css("a").map {|el| el.attribute('href') 
     
   end
 
